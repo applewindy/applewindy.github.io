a8a89756fcbbfacd1dca0ec2eade6732c4264edd
windy <xingxiajianke@hotmail.com>
2019-05-27 00:58:52 +0900

    ff

--- azhar/js/common.js
+++ azhar/js/common.js
@@ -2,4 +2,14 @@ $(function () {
   // $(".dropdown-item").click(function () {
   //   $("#dropdown09.span").css($(this).find("span").css());
   // })
+
+  $('.navToggle').click(function () {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+      $('.globalMenuSp').addClass('active');
+    } else {
+      $('.globalMenuSp').removeClass('active');
+    }
+  });
 });
