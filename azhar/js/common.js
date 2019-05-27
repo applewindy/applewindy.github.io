90eae4ad3e76370f29bfd3451d9ac75f1d5e5f74
Zhu, Yingjie <ts-yingjie.zhu@rakuten.com>
2019-05-27 16:31:58 +0900

    a

--- azhar/js/common.js
+++ azhar/js/common.js
@@ -3,13 +3,4 @@ $(function () {
   //   $("#dropdown09.span").css($(this).find("span").css());
   // })
 
-  $('.navToggle').click(function () {
-    $(this).toggleClass('active');
-
-    if ($(this).hasClass('active')) {
-      $('.globalMenuSp').addClass('active');
-    } else {
-      $('.globalMenuSp').removeClass('active');
-    }
-  });
 });
