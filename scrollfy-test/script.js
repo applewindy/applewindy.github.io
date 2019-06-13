937b13224899e98eecc9c180c00aeef75fd63931
Zhu, Yingjie <ts-yingjie.zhu@rakuten.com>
2019-06-13 15:19:43 +0900

    scrollfy

---
+++ scrollfy-test/script.js
@@ -0,0 +1,58 @@
+jQuery(function($){
+// match height
+$(document).ready(function(){
+    matchHight();
+	offsetScrollOff();
+
+    $(window).resize(function() {
+        matchHight();
+		offsetScrollOff();
+    });
+});
+
+function matchHight() {
+	visual_height = hsize = $(window).height() - 0;
+	$(".match-height").css("height", visual_height + "px");
+}
+function offsetScrollOff() {
+	var off = $('#sec04').offset();
+	console.log('top: ' + off.top);
+	console.log('left: ' + off.left);
+}
+
+
+
+
+// scroll
+var current;
+$(function(){
+	$.scrollify({
+		section:".section",
+		setHeights:false,
+		updateHash:false,
+		before: function(){
+			$('.section').removeClass('active');
+		},
+		after: function(){
+			current = $.scrollify.current();
+			current.addClass('active');
+			
+		if(current === "sec04") {
+			jQuery("#sec04").addClass("hidden");
+		}
+		},
+		//interstitialSection:"header,footer",
+		//touchScroll: false
+	});
+
+
+	$(window).on('resize',function(){
+		$.scrollify.update();
+
+
+
+
+	});
+});	
+});
+
